58d9dfa394405e94e6be82742feb6535d49ec35a
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2022-06-02 18:46:09 -0600

    añadí la lógica para la pantalla de pygame

---
+++ game.py
@@ -0,0 +1,35 @@
+import pygame
+import sys
+
+class Juego:
+
+    def __init__(self, pantalla):
+        
+        #Variables para la lógica del juego
+        self.turnos = 0
+        self.pantalla = pantalla
+
+    def set_tablero(self):
+        pass
+
+    def run(self):
+        pass
+
+
+pygame.init()
+pantalla = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Battleship")
+clock = pygame.time.Clock()
+
+juego = Juego(pantalla)
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pantalla.fill("#000000")
+    pygame.display.update()
+    clock.tick(60)
