8f73ff5a058ba3e2c7b825b3f7403a812ad92580
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 19:10:03 -0600

    .

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ def main():
 
     canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
     canvas.place(x=0, y=0)
+    help_canva = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
+    help_canva.place(x=0, y=0)
 
-    title = tk.Label(canvas, text= "Battleship", font= "sonic-1-hud-font.ttf", bg= "black", fg= "white")
+    title = tk.Label(canvas, text= "Battleship", font= "Sonic 1 HUD Font", bg= "black", fg= "white")
     title.place(x= 100, y= 100)
+
+   
+
+
+    
 
     window.mainloop()
 
