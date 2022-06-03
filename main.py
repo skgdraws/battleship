b40fb01f20f6c46ac6dfdf651d5bde0b3bdcb4af
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 19:35:55 -0600

    .

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def main():
     canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
     canvas.place(x=0, y=0)
     
+    def quit():
+        window.destroy()
+        window.quit
 
-    title = tk.Label(canvas, text= "Battleship", font= "Sonic-1-HUD-Font", bg= "black", fg= "white")
-    title.place(x= 100, y= 100)
+    quit_button= tk.Button(canvas, text= "Quit", command= quit)
+    quit_button.place(x= 100, y= 100)
+    
 
    
 
