d25504a91b4d84b85e55b807241c1eaee033ef32
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 18:54:55 -0600

    title

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import T
 import tkinter as tk
 
 def main():
@@ -8,7 +9,8 @@ def main():
     canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
     canvas.place(x=0, y=0)
 
-    #hola
+    title = tk.Label(main, text= "Battleship", font= "sonic-1-hud-font.ttf", bg= "black", fg= "white")
+    title.place(x= 100, y= 100)
 
     window.mainloop()
 
