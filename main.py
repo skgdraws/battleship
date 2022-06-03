aa23d36115a0aa0969761cda6d14fb9aa6b1fc47
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 20:10:25 -0600

    .

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-import tkinter as tk
+import pygame
 
-def main():
+pygame.init()
 
-    window = tk.Tk()
-    window.geometry("1280x720")
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
 
-    help_canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="white")
-    help_canvas.place(x=0, y=0)
-    canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
-    canvas.place(x=0, y=0)
-    
-    def quit():
-        window.destroy()
-        window.quit
-
-    quit_button= tk.Button(canvas, text= "Quit", command= quit)
-    quit_button.place(x= 100, y= 100)
-    
-
-   
-
-
-    
-
-    window.mainloop()
-
-main()
+def
