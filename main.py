1c9ee659875637179f330d106d1249d39d565465
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 20:25:40 -0600

    .

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
+import sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((1280, 720))
-pygame.display.set_caption("Menu")
 
-def
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
