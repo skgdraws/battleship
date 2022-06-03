9ee8651c7a944e286a67217fb63b35d47561b033
Javier Barraza <jbarraza5010@gmail.com>
2022-06-02 19:31:46 -0600

    .

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     window = tk.Tk()
     window.geometry("1280x720")
 
+    help_canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="white")
+    help_canvas.place(x=0, y=0)
     canvas = tk.Canvas(window,  width=1280, height=720, borderwidth=0, highlightthickness=0, bg="#000000")
     canvas.place(x=0, y=0)
+    
 
     title = tk.Label(canvas, text= "Battleship", font= "Sonic-1-HUD-Font", bg= "black", fg= "white")
     title.place(x= 100, y= 100)
